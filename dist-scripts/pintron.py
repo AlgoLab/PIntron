022e47469bf88cd8884dcb4decc0ca40d52d3d2e
Gianluca Della Vedova <gianluca@dellavedova.org>
2012-06-29 14:12:57 +0200

    Remove requirements that all programs are in PATH
    Now we use the exes infrastructure in case the various subprograms used
    are not in $PATH

--- dist-scripts/pintron.py
+++ dist-scripts/pintron.py
@@ -769,6 +769,7 @@ def pintron_pipeline(options):
                                              "gene-structure",
                                              "compact-compositions",
                                              "maximal-transcripts",
+                                             "ests2bam",
                                              "cds-annotation"
                                              ])
 
@@ -923,7 +924,7 @@ def pintron_pipeline(options):
         json2gtf(options.output_filename, options.extended_gtf_filename, options.genome_filename, options.gene, True)
     if options.output_est_alignments:
         exec_system_command(
-        command="ests2bam --directory=. --genome=" + options.genome_filename,
+        command=exes["ests2bam"] + "ests2bam --directory=. --genome=" + options.genome_filename,
         error_comment="Could not create ESTs-genome alignment file",
         logfile=options.plogfile,
         output_file='est-alignments.sam',
